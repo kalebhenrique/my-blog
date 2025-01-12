b6dd858d44b9eac12a3c92312aa9a013aa9902da
kalebhenrique <kalebhenriquebr@gmail.com>
2025-01-12 16:40:15 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.first_or_initialize(email: "kaleb@example.com")
+user.update!(
+  password: "password",
+  password_confirmation: "password"
+)
